cafa98b7fba34198ab889432d02ab526fd0734b8
Edward Carrion <edwardcarrionp@bcp.com.pe>
2020-07-08 09:16:49 -0500

    feature: change to kotlin dsl

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,10 +2,10 @@ const val kotlinVersion = "1.3.72"
 
 object Build {
     object Versions {
-        const val buildToolsVersion = "4.0.0"
+        const val androidGradlePlugin = "4.0.0"
     }
 
-    const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.buildToolsVersion}"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
 }
 
@@ -28,20 +28,24 @@ object Libraries {
         const val appCompat = "1.1.0"
         const val constraintLayout = "1.1.3"
         const val glide = "4.11.0"
+    }
 
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+    const val coreKtx = "androidx.core:core-ktx:${Versions.ktx}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+}
+
+object TestLibraries {
+    private object Versions {
         const val junit = "4.13"
         const val androidxJunit = "1.1.1"
         const val androidxEspressoCore = "3.2.0"
     }
 
-    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
-    const val coreKtx = "androidx.core:core-ktx:${Versions.ktx}"
-    const val appCompat = "androidx.core:core-ktx:${Versions.appCompat}"
-    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
-    const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
-    const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
-
-    const val junit = "com.github.bumptech.glide:glide:${Versions.junit}"
+    const val junit = "junit:junit:${Versions.junit}"
     const val androidxJunit = "androidx.test.ext:junit:${Versions.androidxJunit}"
     const val androidxEspressoCore = "androidx.test.espresso:espresso-core:${Versions.androidxEspressoCore}"
 }
